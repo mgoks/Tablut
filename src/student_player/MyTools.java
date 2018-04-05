c9a97c2b706e092779869821b66a2db51a488a41
H. Murat Goksel <murat.goksel@mail.mcgill.ca>
2018-04-04 22:14:45 -0400

    bismillah

--- src/student_player/MyTools.java
+++ src/student_player/MyTools.java
@@ -1,7 +1,56 @@
 package student_player;
 
-public class MyTools {
-    public static double getSomething() {
+import boardgame.Move;
+import tablut.TablutBoardState;
+
+public class MyTools implements MinimaxTools
+{
+	public static final int SWEDE     = 1;
+    public static final int MUSCOVITE = 0;
+    public static final int INF		  = Integer.MAX_VALUE;
+    public static final int NEG_INF	  = Integer.MIN_VALUE;
+    
+    public static double getSomething() 
+    {
         return Math.random();
     }
+
+	@Override
+	public Move minimaxDecision(TablutBoardState state) 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int maxValue(TablutBoardState state) 
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int minValue(TablutBoardState state) 
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isTerminal(TablutBoardState state) 
+	{
+		return state.gameOver();
+	}
+
+	@Override
+	public int utility(TablutBoardState state) 
+	{
+		// For now, assume we are Muscovites
+		if (state.getWinner() == MUSCOVITE)
+			return INF;
+		else if (state.getWinner() == SWEDE)
+			return NEG_INF;
+		else
+			return 0;
+	}
 }
--- src/student_player/StudentPlayer.java
+++ src/student_player/StudentPlayer.java
@@ -5,15 +5,17 @@ import tablut.TablutBoardState;
 import tablut.TablutPlayer;
 
 /** A player file submitted by a student. */
-public class StudentPlayer extends TablutPlayer {
+public class StudentPlayer extends TablutPlayer 
+{
 
     /**
      * You must modify this constructor to return your student number. This is
      * important, because this is what the code that runs the competition uses to
      * associate you with your agent. The constructor should do nothing else.
      */
-    public StudentPlayer() {
-        super("xxxxxxxxx");
+    public StudentPlayer() 
+    {
+        super("260572202");
     }
 
     /**
@@ -21,7 +23,8 @@ public class StudentPlayer extends TablutPlayer {
      * object contains the current state of the game, which your agent must use to
      * make decisions.
      */
-    public Move chooseMove(TablutBoardState boardState) {
+    public Move chooseMove(TablutBoardState boardState) 
+    {
         // You probably will make separate functions in MyTools.
         // For example, maybe you'll need to load some pre-processed best opening
         // strategies...
